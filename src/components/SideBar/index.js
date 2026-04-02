09122b0b3406bbf19068ad7579ccc94cd9f32b47
Naresh Jogu <kumarn29346@gmail.com>
2026-04-02 05:09:09 +0000

    Improved Sidebar navigation structure and styling

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -16,58 +16,58 @@ import {
 } from './styledComponents'
 
 const SideBar = () => (
-    <SidebarContainer>
-      <NavList>
-        <NavItem>
-          <NavLink to="/">
-            <AiFillHome size={22} />
-            <p>Home</p>
-          </NavLink>
-        </NavItem>
+  <SidebarContainer>
+    <NavList>
+      <NavItem>
+        <NavLink to="/">
+          <AiFillHome size={22} />
+          <p>Home</p>
+        </NavLink>
+      </NavItem>
 
-        <NavItem>
-          <NavLink to="/trending">
-            <HiFire size={22} />
-            <p>Trending</p>
-          </NavLink>
-        </NavItem>
+      <NavItem>
+        <NavLink to="/trending">
+          <HiFire size={22} />
+          <p>Trending</p>
+        </NavLink>
+      </NavItem>
 
-        <NavItem>
-          <NavLink to="/gaming">
-            <SiYoutubegaming size={22} />
-            <p>Gaming</p>
-          </NavLink>
-        </NavItem>
+      <NavItem>
+        <NavLink to="/gaming">
+          <SiYoutubegaming size={22} />
+          <p>Gaming</p>
+        </NavLink>
+      </NavItem>
 
-        <NavItem>
-          <NavLink to="/saved-videos">
-            <MdPlaylistAdd size={22} />
-            <p>Saved Videos</p>
-          </NavLink>
-        </NavItem>
-      </NavList>
+      <NavItem>
+        <NavLink to="/saved-videos">
+          <MdPlaylistAdd size={22} />
+          <p>Saved Videos</p>
+        </NavLink>
+      </NavItem>
+    </NavList>
 
-      <ContactSection>
-        <ContactHeading>CONTACT US</ContactHeading>
-        <SocialIcons>
-          <SocialImage
-            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png"
-            alt="facebook logo"
-          />
-          <SocialImage
-            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-twitter-logo-img.png"
-            alt="twitter logo"
-          />
-          <SocialImage
-            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png"
-            alt="linked in logo"
-          />
-        </SocialIcons>
-        <ContactText>
-          Enjoy! Now to see your channels and recommendations!
-        </ContactText>
-      </ContactSection>
-    </SidebarContainer>
-  )
+    <ContactSection>
+      <ContactHeading>CONTACT US</ContactHeading>
+      <SocialIcons>
+        <SocialImage
+          src="https://assets.ccbp.in/frontend/react-js/nxt-watch-facebook-logo-img.png"
+          alt="facebook logo"
+        />
+        <SocialImage
+          src="https://assets.ccbp.in/frontend/react-js/nxt-watch-twitter-logo-img.png"
+          alt="twitter logo"
+        />
+        <SocialImage
+          src="https://assets.ccbp.in/frontend/react-js/nxt-watch-linked-in-logo-img.png"
+          alt="linked in logo"
+        />
+      </SocialIcons>
+      <ContactText>
+        Enjoy! Now to see your channels and recommendations!
+      </ContactText>
+    </ContactSection>
+  </SidebarContainer>
+)
 
 export default SideBar
