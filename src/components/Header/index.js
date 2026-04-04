6a50560cf369769aece468ebca5ae8640a061d3e
Naresh Jogu <kumarn29346@gmail.com>
2026-04-04 11:05:11 +0000

    feat(header): add mobile hamburger menu with dropdown navigation links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,12 @@
 import {useState} from 'react'
 import Cookies from 'js-cookie'
-import {useHistory} from 'react-router-dom'
-
-import {FiSun, FiMoon} from 'react-icons/fi'
+import {useHistory, Link} from 'react-router-dom'
+import {FiSun, FiMoon, FiLogOut} from 'react-icons/fi'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiFillHome, AiTwotoneFire} from 'react-icons/ai'
+import {SiYoutubegaming} from 'react-icons/si'
+import {MdPlaylistAdd} from 'react-icons/md'
+import {IoMdClose} from 'react-icons/io'
 
 import {
   HeaderContainer,
@@ -11,15 +15,18 @@ import {
   ThemeButton,
   ProfileImage,
   LogoutButton,
+  IconButton,
+  MobileMenuOverlay,
+  MenuLink,
 } from './styledComponents'
 
 const Header = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false)
+  const [isMenuOpen, setMenuOpen] = useState(false)
   const history = useHistory()
 
-  const toggleTheme = () => {
-    setIsDarkTheme(prev => !prev)
-  }
+  const toggleTheme = () => setIsDarkTheme(prev => !prev)
+  const toggleMenu = () => setMenuOpen(prev => !prev)
 
   const onClickLogout = () => {
     Cookies.remove('jwt_token')
@@ -28,20 +35,59 @@ const Header = () => {
 
   return (
     <HeaderContainer>
-      <Logo
-        src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
-        alt="website logo"
-      />
+      <Link to="/">
+        <Logo
+          src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+          alt="website logo"
+        />
+      </Link>
+
       <NavItems>
         <ThemeButton onClick={toggleTheme}>
-          {isDarkTheme ? <FiSun size={22} /> : <FiMoon size={22} />}
+          {isDarkTheme ? <FiSun size={25} /> : <FiMoon size={25} />}
         </ThemeButton>
+
+        {/* Mobile Hamburger */}
+        <IconButton type="button" onClick={toggleMenu}>
+          <GiHamburgerMenu size={25} />
+        </IconButton>
+
         <ProfileImage
           src="https://assets.ccbp.in/frontend/react-js/nxt-watch-profile-img.png"
           alt="profile"
         />
+
         <LogoutButton onClick={onClickLogout}>Logout</LogoutButton>
+
+        <IconButton type="button" onClick={onClickLogout}>
+          <FiLogOut size={25} />
+        </IconButton>
       </NavItems>
+
+      {/* Mobile Dropdown Overlay */}
+      {isMenuOpen && (
+        <MobileMenuOverlay>
+          <IconButton
+            style={{alignSelf: 'flex-end', marginBottom: '30px'}}
+            onClick={toggleMenu}
+          >
+            <IoMdClose size={30} />
+          </IconButton>
+
+          <MenuLink to="/" onClick={toggleMenu}>
+            <AiFillHome size={20} /> Home
+          </MenuLink>
+          <MenuLink to="/trending" onClick={toggleMenu}>
+            <AiTwotoneFire size={20} /> Trending
+          </MenuLink>
+          <MenuLink to="/gaming" onClick={toggleMenu}>
+            <SiYoutubegaming size={20} /> Gaming
+          </MenuLink>
+          <MenuLink to="/saved-videos" onClick={toggleMenu}>
+            <MdPlaylistAdd size={20} /> Saved videos
+          </MenuLink>
+        </MobileMenuOverlay>
+      )}
     </HeaderContainer>
   )
 }
