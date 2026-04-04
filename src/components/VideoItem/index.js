5f1b9e3fbad5583b87122444b92a5502539e1dba
Naresh Jogu <kumarn29346@gmail.com>
2026-04-04 07:16:30 +0000

    fix(video-item): verify and correct Link path for video details using id

--- src/components/VideoItem/index.js
+++ src/components/VideoItem/index.js
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import {
   VideoCard,
   Thumbnail,
@@ -11,24 +12,26 @@ import {
 
 const VideoItem = props => {
   const {video} = props
-  const {title, thumbnailUrl, viewCount, publishedAt, channel} = video
+  const {title, thumbnailUrl, viewCount, publishedAt, channel, id} = video
 
   return (
-    <VideoCard>
-      <Thumbnail src={thumbnailUrl} alt="video thumbnail" />
+    <Link to={`/videos/${id}`}>
+      <VideoCard>
+        <Thumbnail src={thumbnailUrl} alt="video thumbnail" />
 
-      <VideoInfo>
-        <ChannelLogo src={channel.profileImageUrl} alt="channel logo" />
+        <VideoInfo>
+          <ChannelLogo src={channel.profileImageUrl} alt="channel logo" />
 
-        <VideoDetails>
-          <Title>{title}</Title>
-          <ChannelName>{channel.name}</ChannelName>
-          <VideoMeta>
-            {viewCount} views • {publishedAt}
-          </VideoMeta>
-        </VideoDetails>
-      </VideoInfo>
-    </VideoCard>
+          <VideoDetails>
+            <Title>{title}</Title>
+            <ChannelName>{channel.name}</ChannelName>
+            <VideoMeta>
+              {viewCount} views • {publishedAt}
+            </VideoMeta>
+          </VideoDetails>
+        </VideoInfo>
+      </VideoCard>
+    </Link>
   )
 }
 
