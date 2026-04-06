ea5e24414ca2d05dc6beb304e785dd656bc384fc
Naresh Jogu <kumarn29346@gmail.com>
2026-04-06 10:15:41 +0000

    feat(App): integrate NxtWatchContext and implement saved videos functionality across components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
 import {Switch, Route} from 'react-router-dom'
 import {useState} from 'react'
 import Login from './components/Login'
-
 import Home from './components/Home'
 import Trending from './components/Trending'
 import Gaming from './components/Gaming'
 import NotFound from './components/NotFound'
-import SavedVideos from './components/SavedVideos'
-import ThemeContext from './context/ThemeContext'
 import ProtectedRoute from './components/ProtectedRoute'
 import VideoItemDetails from './components/VideoItemDetails'
 import GamingItemDetails from './components/GamingItemDetails'
+import NxtWatchContext from './context/NxtWatchContext'
+import SavedVideos from './components/SavedVideos'
 
 import './App.css'
 
 // Replace your code here
 const App = () => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
+  const [savedVideos, setSavedVideos] = useState([])
 
-  const toggleTheme = () => {
-    setIsDarkTheme(prev => !prev)
+  const addOrRemoveVideo = video => {
+    setSavedVideos(prevVideos => {
+      const isAlreadySaved = prevVideos.find(each => each.id === video.id)
+
+      if (isAlreadySaved) {
+        return prevVideos.filter(each => each.id !== video.id)
+      }
+
+      return [...prevVideos, video]
+    })
   }
 
   return (
-    <ThemeContext.Provider value={{isDarkTheme, toggleTheme}}>
+    <NxtWatchContext.Provider value={{savedVideos, addOrRemoveVideo}}>
       <Switch>
         <Route exact path="/login" component={Login} />
-
         <ProtectedRoute exact path="/" component={Home} />
         <ProtectedRoute exact path="/trending" component={Trending} />
         <ProtectedRoute exact path="/gaming" component={Gaming} />
         <ProtectedRoute exact path="/videos/:id" component={VideoItemDetails} />
+        <ProtectedRoute exact path="/saved-videos" component={SavedVideos} />
         <ProtectedRoute
           exact
           path="/gaming/:id"
           component={GamingItemDetails}
         />
-        <ProtectedRoute exact path="/saved-videos" component={SavedVideos} />
-
         <Route component={NotFound} />
       </Switch>
-    </ThemeContext.Provider>
+    </NxtWatchContext.Provider>
   )
 }
 
