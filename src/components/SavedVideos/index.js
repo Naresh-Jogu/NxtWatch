ae09ea15b17f83e66ce3096837338c13329d14cb
Naresh Jogu <kumarn29346@gmail.com>
2026-04-06 09:51:57 +0000

    feat(SavedVideos): implement SavedVideos component

--- src/components/SavedVideos/index.js
+++ src/components/SavedVideos/index.js
@@ -1,9 +1,49 @@
+import {useContext} from 'react'
+
+import NxtWatchContext from '../../context/NxtWatchContext'
+import VideoItem from '../VideoItem'
 import Layout from '../Layout'
 
-const SavedVideos = () => (
-  <Layout>
-    <h1>Saved Videos</h1>
-  </Layout>
-)
+import {
+  SavedVideosContainer,
+  SavedVideosList,
+  NoSavedVideosContainer,
+  NoSavedVideosHeading,
+  NoSavedVideosText,
+  FailureImage,
+} from './styledComponents'
+
+const SavedVideos = () => {
+  const {savedVideos} = useContext(NxtWatchContext)
+
+  if (!savedVideos || savedVideos.length === 0) {
+    return (
+      <Layout>
+        <NoSavedVideosContainer>
+          <FailureImage
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-no-saved-videos-img.png"
+            alt="failure view"
+          />
+          <NoSavedVideosHeading>No saved videos found</NoSavedVideosHeading>
+          <NoSavedVideosText>
+            You can save videos while watching them
+          </NoSavedVideosText>
+        </NoSavedVideosContainer>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout>
+      <SavedVideosContainer>
+        <SavedVideosList>
+          {savedVideos.map(video => (
+            <VideoItem key={video.id} video={video} isSavedPage />
+          ))}
+        </SavedVideosList>
+      </SavedVideosContainer>
+    </Layout>
+  )
+}
 
 export default SavedVideos
